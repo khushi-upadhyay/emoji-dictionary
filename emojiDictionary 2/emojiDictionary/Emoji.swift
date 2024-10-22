dc9adb48b665ad3fba58e46ab6eb2c785ec13752
Khushi <109527041+khushi-upadhyay@users.noreply.github.com>
2024-10-23 01:26:31 +0530

    Add files via upload

---
+++ emojiDictionary 2/emojiDictionary/Emoji.swift
@@ -0,0 +1,13 @@
+//
+//  Emoji.swift
+//  emojiDictionary
+//
+//  Created by admin47 on 01/10/24.
+//
+
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
---
+++ emojiDictionary 2/emojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emojiDictionary
+//
+//  Created by admin39 on 01/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
